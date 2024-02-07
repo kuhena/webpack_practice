48e1a210e56369d7281b799b811b5b890844e01b
kuhena <pennant25video@gmail.com>
2024-02-07 15:22:52 +0900

    29. style-loaderでCSSのスタイルを適応させる

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -4,5 +4,21 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-    }
+        filename: 'main.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',//loaderは下から上に適応
+                    },
+                ],
+            },
+        ],
+    },
 }
