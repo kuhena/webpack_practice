7bc923d1b8dbdd7e1419781b6be481040f073de1
kuhena <pennant25video@gmail.com>
2024-02-08 15:55:21 +0900

    62. Babelを使ってES6をトランスパイルする

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -15,6 +15,18 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader:'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                        },
+                    },
+                ],
+            },
+            {
                 test: /\.(css|sass|scss)/,
                 use: [
                     {
